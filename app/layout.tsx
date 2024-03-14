d94dd6fa1df85ad7b25fbba35e3d0b7c7dd7371e
Brandon Roberts <brandon.roberts@friday.de>
2024-03-14 14:30:04 +0100

    create configure amplify client side

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import ConfigureAmplifyClientSide from "@/components/ConfigureAmplify";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ConfigureAmplifyClientSide />
+        {children}
+      </body>
     </html>
   );
 }
